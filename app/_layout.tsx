ceba4eddd7fb4b17f47cab5806e5873bf41c4a81
Mehdi Marzban <marzban7272@gmail.com>
2025-03-17 22:22:51 +0330

    feat: add react-native-safearea-context and light and dark color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,68 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import "@/global.css";
+import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import "react-native-reanimated";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import "@/global.css";
+import { useNativeWindColorScheme } from "@/hooks/useNativeWindColorScheme";
+import { NAV_THEME } from "@/constants/ThemeColors";
+
+const LIGHT_THEME: Theme = {
+    ...DefaultTheme,
+    colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+    ...DarkTheme,
+    colors: NAV_THEME.dark,
+};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const { colorScheme, isDarkColorScheme } = useNativeWindColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    const hasMounted = useRef(false);
+    const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+    //* color scheme loaded
+    useEffect(() => {
+        if (hasMounted.current) {
+            return;
+        }
+        setIsColorSchemeLoaded(true);
+        hasMounted.current = true;
+    }, []);
+
+    //* font loaded
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded || !isColorSchemeLoaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+            <SafeAreaProvider>
+                <SafeAreaView className="flex-1">
+                    <Stack>
+                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                        <Stack.Screen name="+not-found" />
+                    </Stack>
+                    <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+                </SafeAreaView>
+            </SafeAreaProvider>
+        </ThemeProvider>
+    );
 }
